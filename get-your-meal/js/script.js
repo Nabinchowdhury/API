eb340a193e9810e6c0c7df10b41fb2ef069a3a63
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-08-29 03:28:55 +0600

    updated

--- get-your-meal/js/script.js
+++ get-your-meal/js/script.js
@@ -19,7 +19,7 @@ const showFood = (foods) => {
         // console.log(food)
         foodItems.innerHTML =
             `
-           <div class="card">
+           <div class="card" onclick="loadSelectedFood(${food.idMeal})">
            <img src="${food.strMealThumb}" class="card-img-top" alt="...">
            <div class="card-body">
            <h5 class="card-title">${food.strMeal}</h5>
@@ -39,5 +39,31 @@ const searchFood = () => {
     serched.value = ""
     // console.log(searchedFood)
 }
+const loadSelectedFood = (id) => {
+
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+    // console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showSelectedFood(data.meals[0]))
+}
+
+const showSelectedFood = meal => {
+    // console.log(meal)
+    const foodDetails = document.getElementById("food-detail")
+    foodDetails.innerHTML = ``;
+    const selectedFood = document.createElement("div")
+    selectedFood.classList.add("card")
+    selectedFood.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card - img - top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+        `;
+    foodDetails.appendChild(selectedFood)
+}
 
 loadFood("");
+
