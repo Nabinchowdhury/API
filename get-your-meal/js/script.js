743d5d0d668b08259718280251e3065c5755e2cb
Nabinchowdhury <nabinchowdhury26@gmail.com>
2022-08-29 02:00:59 +0600

    updated

---
+++ get-your-meal/js/script.js
@@ -0,0 +1,42 @@
+
+const loadFood = (search) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showFood(data.meals))
+}
+
+const showFood = (foods) => {
+    const foodContainer = document.getElementById('food-container')
+    foodContainer.innerHTML = ``;
+
+    foods.forEach(food => {
+
+        const foodItems = document.createElement('div')
+        foodItems.classList.add("text-center")
+        console.log(food)
+        foodItems.innerHTML =
+            `
+    <div class="card">
+                    <img src="${food.strMealThumb}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${food.strMeal}</h5>
+                        <p class="card-text">${food.strInstructions.slice(0, 100)}</p>
+                      
+                    </div>
+                </div>
+    `
+        foodContainer.appendChild(foodItems)
+    })
+}
+
+
+const searchFood = () => {
+    const serched = document.getElementById('search-bar')
+    const searchedFood = serched.value
+    loadFood(searchedFood);
+    serched.value = ""
+    // console.log(searchedFood)
+}
+
+loadFood("");
